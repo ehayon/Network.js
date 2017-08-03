e4e15d27ad9ac529f4a1b78e80c6aca3b39e91bd
Ethan Hayon <ehayon@gmail.com>
2017-08-03 15:01:46 -0400

    add midpoint to edge

--- src/Graph/edge.js
+++ src/Graph/edge.js
@@ -1,17 +1,41 @@
 import React from 'react'
-import {Line} from 'react-konva'
+import {Group,Line,Circle} from 'react-konva'
 
 export default class Edge extends React.Component {
+    /* TODO: Make sure duplicate edges are
+             not stacked top of eachother */
+
     render() {
         /* Draw an edge between props.source and props.target */
-        return <Line 
-            points={[
-                this.props.source.x,
-                this.props.source.y,
-                this.props.target.x,
-                this.props.target.y
-            ]}
-            stroke='red'
-            />
+        
+        // Calculate midpoint of the line
+        let midpoint = {}
+        midpoint.x = (
+            Math.floor((this.props.source.x + this.props.target.x) / 2)
+        )
+         midpoint.y = (
+            Math.floor((this.props.source.y + this.props.target.y) / 2)
+        )
+        return (
+            <Group>
+                <Line 
+                    points={[
+                        this.props.source.x,
+                        this.props.source.y,
+                        this.props.target.x,
+                        this.props.target.y
+                    ]}
+                    stroke='red'
+                />
+                <Circle 
+                    radius={this.props.radius || 5}
+                    fill={'blue'}
+                    x={midpoint.x}
+                    y={midpoint.y}
+                    stroke={'black'}
+                    strokeWidth={1}
+                />
+            </Group>
+        )
     }
 }
