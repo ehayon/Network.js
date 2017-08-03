851f71d744c871d5ed2a6d75400217747ca15bf7
Ethan Hayon <ehayon@gmail.com>
2017-08-03 17:40:39 -0400

    some math to find bezier anchor point

--- src/Graph/edge.js
+++ src/Graph/edge.js
@@ -8,7 +8,7 @@ export default class Edge extends React.Component {
     render() {
         /* Draw an edge between props.source and props.target */
         
-        // Calculate midpoint of the line
+        // find midpoint of the line
         let midpoint = {}
         midpoint.x = (
             Math.floor((this.props.source.x + this.props.target.x) / 2)
@@ -16,6 +16,24 @@ export default class Edge extends React.Component {
          midpoint.y = (
             Math.floor((this.props.source.y + this.props.target.y) / 2)
         )
+        // find negative reciprocal of slope => perpendicular bisector to midpoint
+        // -(x2-x1)/(y2-y1)
+        const invSlope = -1 *
+            (this.props.target.x - this.props.source.x) / 
+            (this.props.target.y - this.props.source.y)
+        
+        const anchorDistance = -50
+        let anchorPt = {}
+        
+        if (invSlope == -Infinity || invSlope == Infinity) {
+            anchorPt.x = midpoint.x
+            anchorPt.y = midpoint.y + anchorDistance
+        } else {
+            let x_p = (anchorDistance) / Math.sqrt(1 + Math.pow(invSlope, 2))
+            anchorPt.x = midpoint.x + x_p
+            anchorPt.y = midpoint.y + (invSlope * x_p)
+        }
+
         return (
             <Group>
                 <Line 
@@ -34,6 +52,15 @@ export default class Edge extends React.Component {
                     y={midpoint.y}
                     stroke={'black'}
                     strokeWidth={1}
+                />
+                <Line 
+                    points={[
+                        midpoint.x,
+                        midpoint.y,
+                        anchorPt.x,
+                        anchorPt.y
+                    ]}
+                    stroke='blue'
                 />
             </Group>
         )
