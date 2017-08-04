f6077722f10d28ef66b4bddd8a9c9044e6370b6a
Ethan Hayon <ehayon@gmail.com>
2017-08-03 21:37:56 -0400

    fix bad indentation

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import store from './store'
 import Graph from './Graph'
 
 for(var i=0; i<2; i++) {
-    store.dispatch({
-      type: "ADD_NODE",
-      name: "Node " + i,   
-      id: i,
-      x: Math.floor(Math.random()*700),
-      y: Math.floor(Math.random()*700)      
-    })
+  store.dispatch({
+    type: "ADD_NODE",
+    name: "Node " + i,   
+    id: i,
+    x: Math.floor(Math.random()*700),
+    y: Math.floor(Math.random()*700)      
+  })
 }
 for(var i=0; i<1; i++) {
   store.dispatch({
